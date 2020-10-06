cb0d046aa387b6e04cd091ee997e29d1cd89e031
govardhana <42324366+naidugova@users.noreply.github.com>
2020-10-06 22:17:51 +0530

    Working with npm packages

---
+++ frame-print/index.js
@@ -0,0 +1,6 @@
+//the below line is very important which will export our top level api
+module.exports = function print(msg) {
+    console.log('***************');
+    console.log(msg);
+    console.log('***************');
+}
