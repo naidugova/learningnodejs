97da4b4a322497ce85d4c6cf0eaf036e2b4eb569
govardhana <42324366+naidugova@users.noreply.github.com>
2020-10-14 01:19:55 +0530

    A simple task manager using events

---
+++ TaskListManager/client.js
@@ -0,0 +1,23 @@
+const EventEmitter = require('events');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+const client = new EventEmitter();
+const server = require('./server')(client);
+
+server.on('response',(resp)=>{
+    //process.stdout.write('\u001B[2]\u001B[0;f');
+
+    process.stdout.write(resp);
+
+    process.stdout.write('\n\>');
+});
+let command, args;
+rl.on('line',(input)=>{
+    [command,...args] = input.split(' ');
+    client.emit('command',command,args);
+});
